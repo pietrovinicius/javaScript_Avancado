7a040f9f0a86171f2093cf671b5ae83601dfb89a
pietro lima <pietro.lima@hotmail.com>
2019-01-21 08:39:20 -0200

    Function auto-invocável IIFE

--- client/js/app/views/NegociacoesView.js
+++ client/js/app/views/NegociacoesView.js
@@ -1,13 +1,11 @@
 class NegociacoesView {
 
     constructor(elemento) {
-
         this._elemento = elemento;
     }
 
     _template(model) {
         //tive que usar a templateString ``
-
         return `
         <table class="table table-hover table-bordered">
         <thead>
@@ -20,19 +18,24 @@ class NegociacoesView {
         </thead>
 
         <tbody>
-            ${model.negociacoes.map( n => { 
-                return `
+            ${model.negociacoes.map(n => `
                 <tr>
                     <td>${DateHelper.dataParaTexto(n.data)}</td>
                     <td>${n.quantidade}</td>
                     <td>${n.valor}</td>
                     <td>${n.volume}</td>
                 </tr>
-                `
-            }).join('')}
+                `).join('')}
         </tbody>
-
         <tfoot>
+        <td colspan="3"></td>
+        <td>${
+            (function(){
+                let total = 0;
+                model.negociacoes.forEach(n => total += n.volume);
+                return total;
+            })()
+        }</td>
         </tfoot>
     </table>
         `;
